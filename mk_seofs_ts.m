f4fe721874345bbefdff8e8c71ebb226fb696a12
Dan Amrhein <damrhein@ucar.edu>
2020-10-27 15:29:12 -0600

    initial commit

---
+++ mk_seofs_ts.m
@@ -0,0 +1,66 @@
+function[EOF_ts,SV_ts,t] = mk_seofs_ts(data,time,neofs,binsize)
+    % Function to compute "snapshot" projections of patterns onto ensemble
+    % model output
+    % INPUTS
+    % data      Modal data matrix with dimensions (space,time,nens)
+    % time      Time axis of model output
+    % patts     Matrix of spatial patterns (space along rows) to project
+    % binsize   Odd (symmetric) length of bin in time in which to compute
+    %           SEOFs
+    %
+    % OUTPUTS
+    % EOF_ts    Time series (indexed by space, EOF index, time) of leading
+    %           EOFs
+    % SV_ts     Time series (indexed by EOF index, time) of leading
+    %           singular values
+    % t         Time corresponding to EOF_ts and SV_ts
+    %
+    % We throw away padding at the beginning and end so that all
+    % computations use the same number of times.
+    %
+    % DEA 10/20
+    
+    pad                = floor(binsize/2);
+    [~,td,~]           = size(data);
+    
+    t                  = [];
+    EOF_ts             = [];
+    SV_ts              = [];
+    
+    for ii = (pad+1):(td-pad)
+        
+        % Define a time range
+        tr             = (ii-pad):(ii+pad);
+        
+        % Select time window
+        dat            = data(:,tr,:);
+        
+        % Compute weights and save
+        [sU,sS]        = mk_seofs(dat,neofs);
+        EOF_ts(:,:,ii) = sU;
+        SV_ts(:,ii)   = sS;
+        t(ii)          = time(ii);
+    end
+
+end
+
+function [sU,sS] = mk_seofs(dat,neofs)
+    % Function to compute "snapshot" EOFs from ensemble model output
+    %
+    % INPUTS
+    % data      Modal data matrix with dimensions (space,time,nens)
+    % neofs     Number of snapshot EOFs to output as a function of time.
+    %
+    % OUTPUTS
+    % sU        Left singular vectors (EOFs) computed over ensemble number
+    %           and time for this particular time bin
+    % sS        Same as sU, but a vector of singular values
+
+    [sd,td,nd]   = size(dat);
+    datnm        = reshape(dat,sd,td*nd)-mean(reshape(dat,sd,td*nd),2);
+    [sUa,sSa,~]  = svd(datnm,'econ');
+    sU           = sUa(:,1:neofs);
+    sS           = sSa(1:neofs);
+
+end
+
---
+++ mk_sproj_ts.m
@@ -0,0 +1,62 @@
+function[wts_ts,t] = mk_sproj_ts(data,time,patts,binsize)
+    % Function to compute "snapshot" projections of patterns onto ensemble
+    % model output
+    % INPUTS
+    % data      Modal data matrix with dimensions (space,time,nens)
+    % time      Time axis of model output
+    % patts     Matrix of spatial patterns (space along rows) to project
+    % binsize   Odd (symmetric) length of bin in time in which to compute
+    %           SEOFs
+    %
+    % OUTPUTS
+    % wts_ts    Time series (indexed by column) of weights corresponding to
+    %           different patterns (indexed by row)
+    % t         Time for weight time series
+    %
+    % We throw away padding at the beginning and end so that all
+    % computations use the same number of times.
+    %
+    % DEA 10/20
+    
+    pad              = floor(binsize/2);
+    [~,td,~]         = size(data);
+    
+    t                = [];
+    wts_ts           = [];
+    
+    for ii = (pad+1):(td-pad)
+        % Define a time range
+        tr           = (ii-pad):(ii+pad);
+        
+        % Select time window
+        dat          = data(:,tr,:);
+        
+        % Compute weights and save
+        wts_ts(:,ii) = mk_sproj(dat,patts);
+        t(ii)        = time(ii);
+    end
+
+end
+
+function [wts] = mk_sproj(dat,patts)
+    % Function to compute "snapshot" projections of patterns onto ensemble
+    % model output
+    % INPUTS
+    % data      Modal data matrix with dimensions (space,time,nens)
+    % patts     Matrix of spatial patterns (space along rows) to project
+    %
+    % OUTPUTS
+    % wts       Vector of SVD-like weights computed for each pattern by
+    %           summing projections of ensemble number and time
+    %
+    % Goal is to get a weighting like an EOF. So the procedure is to
+    % project each pattern onto the data, ending up with a vector (indexed
+    % by time and nens). Then normalize that vector so that v'v*v = 1 (like
+    % a PC). The normalization is the weight.
+
+    [sd,td,nd] = size(dat);
+    datnm      = reshape(dat,sd,td*nd)-mean(reshape(dat,sd,td*nd),2);
+    proj       = datnm'*patts;
+    wts        = sqrt(sum(proj.*proj,2));
+
+end
